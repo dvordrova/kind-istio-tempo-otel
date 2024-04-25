eb60e0c2e099b72643caeb63276add7e8acbcb37
Dmitry Bozhko <meridos.programs@yandex.ru>
2024-04-26 03:30:40 +0400

    fix appProtocol patcher

--- terraform/scripts/tempo-add-app-protocols.py
+++ terraform/scripts/tempo-add-app-protocols.py
@@ -3,17 +3,15 @@ import yaml
 import sys
 
 def modify_yaml(yaml_data):
-    # Load the YAML data
     docs = yaml.safe_load_all(yaml_data)
+    modified_docs = []
 
     for doc in docs:
-        if doc['kind'] != 'Service':
-            continue
-        for port in doc.get('spec', {}).get('ports', []):
-            port['appProtocol'] = port.get('appProtocol', '') or 'tcp'
-        # Output the modified document
-    yaml.dump(doc, sys.stdout)
-
+        if doc['kind'] == 'Service':
+            for port in doc.get('spec', {}).get('ports', []):
+                port['appProtocol'] = port.get('appProtocol', '') or 'tcp'
+        modified_docs.append(doc)
+    yaml.dump_all(modified_docs, sys.stdout, default_flow_style=False, sort_keys=False)
 
 if __name__ == '__main__':
     modify_yaml(sys.stdin.read())

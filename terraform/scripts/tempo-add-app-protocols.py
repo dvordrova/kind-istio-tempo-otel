68e905cc0760326d12523599fbaad377e893023c
Dmitry Bozhko <meridos.programs@yandex.ru>
2024-04-27 03:04:15 +0400

    fix tempo istio problem, probably

--- terraform/scripts/tempo-add-app-protocols.py
+++ terraform/scripts/tempo-add-app-protocols.py
@@ -2,16 +2,28 @@
 import yaml
 import sys
 
+def add_app_protocol(service):
+    for port in service.get('spec', {}).get('ports', []):
+        port['appProtocol'] = port.get('appProtocol', '') or 'tcp'
+
+def modify_service(doc, mapper):
+    if doc['kind'] == 'Service':
+        mapper(doc)
+
 def modify_yaml(yaml_data):
+    with open("grafana-tempo-distributed.yaml", "w") as f:
+        f.write(yaml_data)
+
+    yaml_data = yaml_data.replace('dns+grafana-tempo-distributed-gossip-ring:7946', 'dns+grafana-tempo-distributed-gossip-ring.monitoring.svc.cluster.local:7946')
     docs = yaml.safe_load_all(yaml_data)
     modified_docs = []
-
     for doc in docs:
-        if doc['kind'] == 'Service':
-            for port in doc.get('spec', {}).get('ports', []):
-                port['appProtocol'] = port.get('appProtocol', '') or 'tcp'
+        modify_service(doc, add_app_protocol)
         modified_docs.append(doc)
-    yaml.dump_all(modified_docs, sys.stdout, default_flow_style=False, sort_keys=False)
+    yaml_dump = yaml.dump_all(modified_docs, default_flow_style=False, sort_keys=False)
+    with open("grafana-tempo-distributed-modified.yaml", "w") as f:
+        f.write(yaml_dump)
+    sys.stdout.write(yaml_dump)
 
 if __name__ == '__main__':
     modify_yaml(sys.stdin.read())

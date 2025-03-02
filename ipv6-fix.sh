2cd070733b4b943dd1845024d1f01206c8a7f0c8
Dmitry Bozhko <meridos.programs@yandex.ru>
2025-03-03 02:38:23 +0400

    fix config overwrite [2]

--- ipv6-fix.sh
+++ ipv6-fix.sh
@@ -4,10 +4,11 @@
 if [ ! -f /etc/docker/daemon.json ];
     then echo "{}" > /etc/docker/daemon.json;
 fi;
-jq ' . += { "ip6tables": false }' /etc/docker/daemon.json > /etc/docker/daemon.json;
+echo $(jq ' . += { "ip6tables": false }' /etc/docker/daemon.json) > /etc/docker/daemon.json;
 
 # restart dockerd
 DOCKERD_CMD=$(ps aux | grep [d]ockerd | awk 'NF{print $(NF-2),$(NF-1),$NF}');
 pkill dockerd && \
     while pgrep -l dockerd; do sleep 1; done;
+echo "RUN $DOCKERD_CMD"
 $DOCKERD_CMD > /dev/null 2>&1 &

fd917ef270be8b368995126ceadb338516b1eb1a
Dmitry Bozhko <meridos.programs@yandex.ru>
2025-03-03 02:06:09 +0400

    fix dockerd args

--- ipv6-fix.sh
+++ ipv6-fix.sh
@@ -7,6 +7,6 @@ fi;
 jq ' . += { "ip6tables": false }' /etc/docker/daemon.json;
 
 # restart dockerd
-ps aux | grep [d]ockerd | awk 'NF{print "sudo",$(NF-2),$(NF-1),$NF,"$"}' > prepare.sh;
+ps aux | grep [d]ockerd | awk 'NF{print "sudo",$(NF-2),$(NF-1),$NF,"&"}' > prepare.sh;
 pkill dockerd;
 sh prepare.sh;
